27ac9fdcd9d850451ecf1a9907af00473e4b05a4
César Uriarte <cesar.uriarte@unmsm.edu.pe>
2024-10-12 18:46:55 -0500

    Se crea caracteristica de enviar correo cuando existe cierta cantidad de errores en un tiempo determinado

--- src/main/env.d.ts
+++ src/main/env.d.ts
@@ -4,6 +4,11 @@ interface ImportMetaEnv {
   readonly MAIN_VITE_URL: string
   readonly MAIN_VITE_PASS: string
   readonly MAIN_VITE_DRIVE:string
+  readonly MAIN_VITE_EMAIL: string
+  readonly MAIN_VITE_PASSEMAIL: string
+  readonly MAIN_VITE_EMAILRECEP: string
+  readonly MAIN_VITE_TIMEERRORESSEND: number
+  readonly MAIN_VITE_ERRORCOUNTSEND: number
 }
 
 interface ImportMeta {
--- src/main/services/logErrorToFile.ts
+++ src/main/services/logErrorToFile.ts
@@ -1,6 +1,8 @@
 import { app } from 'electron'
 import { promises as fs } from 'fs'
 import path from 'path'
+import { transporter } from './sendEmail'
+import os from 'os'
 
 async function ensureDirectoryExistence(filePath: string) {
   const dirname = path.dirname(filePath)
@@ -10,6 +12,10 @@ async function ensureDirectoryExistence(filePath: string) {
     console.error('Error al crear la carpeta:', err)
   }
 }
+
+let errorCount: number = 0
+let firstErrorTime: null | number = null
+const listErrors: Array<string> = []
 
 export async function logErrorToFile(errorMessage: string) {
   const appDataPath = app.getPath('userData')
@@ -23,7 +29,50 @@ export async function logErrorToFile(errorMessage: string) {
   try {
     await fs.appendFile(logFilePath, logMessage)
     console.log('Error registrado en el archivo log.')
+
+    const now = Date.now()
+
+    if (!firstErrorTime) {
+      firstErrorTime = now
+    }
+
+    if (now - firstErrorTime > import.meta.env.MAIN_VITE_TIMEERRORESSEND * 60 * 1000) {
+      errorCount = 1
+      firstErrorTime = now
+      listErrors.length = 0
+    } else {
+      errorCount++
+      listErrors.push(errorMessage)
+    }
+
+    if (errorCount >= import.meta.env.MAIN_VITE_ERRORCOUNTSEND) {
+      const response = await transporter.sendEmail(
+        import.meta.env.MAIN_VITE_EMAILRECEP,
+        'ERROR EN EL SERVIDOR ' + getPrivateIP(),
+        'Se han generado errores consecutivos: ',
+        listErrors
+      )
+      errorCount = 0
+      firstErrorTime = null
+      listErrors.length = 0
+      if (!response.success) logErrorToFile('El mensaje no se ha podido enviar al correo electrónico')
+    }
   } catch (err) {
     console.error('No se pudo escribir en el archivo log:', err)
   }
+}
+
+const getPrivateIP = (): null | string => {
+  const interfaces = os.networkInterfaces()
+  for (const iface of Object.values(interfaces)) {
+    if (iface) {
+      for (const alias of iface) {
+        if (alias.family === 'IPv4' && !alias.internal) {
+          return alias.address as string
+        }
+      }
+    }
+  }
+
+  return null
 }
